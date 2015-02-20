df17ee6b3217572848fa9078f0f46598f69aa454
ghidra <ghidra@gmail.com>
2015-02-20 18:53:31 -0500

    added to do notes

--- pygif.py
+++ pygif.py
@@ -2,17 +2,35 @@ from images2gif import writeGif
 from PIL import Image, ImageSequence
 import sys 
 import os
+import math
 
 from os import listdir
 from os.path import isfile, join
 
+#i need to consider exr files
+#just use rvio to deal with those
+#https://docs.python.org/2/library/subprocess.html
+#rvio file.*.exr -o file.#.jpg
+#i should also not add in every file, for file size
+#do the math for the time between frames
 
-def main(openfolder,savefolder,filename,sizex,sizey):
+#this also expects file to be name.frame.ext ie image.1.png
+
+
+def main(openfolder,savefolder,filename,sizex,sizey,fps=24.0,max_frames=72):
 	seq=[]
 	size = sizex, sizey
 	#kwarg[1] should be the folder of the sequence
-	onlyfiles = [ f for f in listdir(openfolder) if isfile(join(openfolder,f)) ]
+	onlyfiles = [ f for f in sorted(listdir(openfolder)) if isfile(join(openfolder,f)) ]
+	found = len(onlyfiles)
+	mod = math.ceil(float(found)/float(max_frames))
 	for img in onlyfiles:
+		#loop the found files to weed out any that I cant use
+		#splitim = img.split(".")
+		# we ignore gif, because we assume they were what we craeted already, wo renders gif sequences
+		#also ignore images that are past our mod
+		#if(splitim[len(splitim)-1]!="gif" and float(splitim[len(splitim)-2])%mod == 0 ):
+
 		#lets make the smaller first just incase
 		try:
 			splitim = img.split(".")
